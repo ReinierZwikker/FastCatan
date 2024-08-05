504a69f9635972032dc8aa0eddb39e5f8f320e98
Reinier Zwikker <r.j.n.zwikker@student.tudelft.nl>
2024-08-05 12:45:56 +0200

    Add random player

---
+++ src/game/AIPlayer/random_player.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <iostream>
+
+#include "random_player.h"
+
+RandomPlayer::RandomPlayer(Player *connected_player) {
+  player = connected_player;
+  console_tag = color_name(connected_player->player_color) + "> " + color_offset(connected_player->player_color);
+  player_print("Hello World! I am player number " + std::to_string(color_index(player->player_color) + 1) + "!\n");
+}
+
+void RandomPlayer::player_print(std::string text) {
+  printf("%s%s", console_tag.c_str(), text.c_str());
+}
+
+Move RandomPlayer::get_move(Board *board, int cards[5]) {
+  Move selected_move;
+
+  player_print("My Cards:\n");
+
+  for (int card_i = 0; card_i < 5; ++card_i) {
+    player_print("    " + card_name(index_card(card_i)) + " = " + std::to_string(cards[card_i]) + "\n");
+  }
+
+  player_print("My possible moves:\n");
+
+  int move_i;
+
+  for (move_i = 0; move_i < max_available_moves; ++move_i) {
+    if (player->available_moves[move_i].move_type == NoMove) {
+      break;
+    }
+    player_print("Move " + std::to_string(move_i + 1) + ": " + move2string(player->available_moves[move_i]) + "\n");
+  }
+
+
+  int selected_move_i;
+
+  if (move_i <= 1) {
+    selected_move_i = 0;
+  } else {
+    std::mt19937 gen(randomDevice());
+    std::uniform_int_distribution<> random_move(0, move_i-1);
+
+    selected_move_i = random_move(gen);
+
+  }
+
+  selected_move = player->available_moves[selected_move_i];
+
+  player_print("Selecting a random move: " + std::to_string(selected_move_i) + "\n");
+
+
+  player_print("\nSelected move: " + move2string(selected_move) + "\n");
+  return selected_move;
+}
+
+void RandomPlayer::finish_round(Board *board) {
+
+}
+
+RandomPlayer::~RandomPlayer() {
+  printf("Thank you for playing!\n");
+}
---
+++ src/game/AIPlayer/random_player.h
@@ -0,0 +1,36 @@
+#ifndef FASTCATAN_HUMAN_PLAYER_H
+#define FASTCATAN_HUMAN_PLAYER_H
+
+#include "../components.h"
+#include "../board.h"
+#include "../player.h"
+
+#include <random>
+#include <string>
+
+
+class RandomPlayer : public PlayerAgent {
+public:
+  explicit RandomPlayer(Player *connected_player);
+  Move get_move(Board *board, int cards[5]) override;
+  void finish_round(Board *board) override;
+  inline void unpause(Move move) override {};
+
+  void player_print(std::string text);
+
+  inline PlayerType get_player_type() override { return player_type; }
+  inline PlayerState get_player_state() override { return player_state; }
+
+  std::random_device randomDevice;
+
+  ~RandomPlayer();
+
+private:
+  Player *player;
+  const PlayerType player_type = NNPlayer;
+  const PlayerState player_state = Waiting;
+  std::string console_tag;
+};
+
+
+#endif //FASTCATAN_HUMAN_PLAYER_H
